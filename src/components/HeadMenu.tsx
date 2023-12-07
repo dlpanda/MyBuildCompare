85c520cc4a431b19aba5a6bb1ace0e37b530aa9b
潘嘉岩 <jiayan.pan@ctvic.cn>
2023-12-07 15:27:43 +0800

    测试vercel不更新问题

--- src/components/HeadMenu.tsx
+++ src/components/HeadMenu.tsx
@@ -1,28 +1,6 @@
-import Image from 'next/image'
-import Link from "next/link"
-// 图片
-import Logo from '../assets/index/logo.png'
-import SearchGrey from '../assets/icon/search-grey.png'
-import '../style/index.css'
-
 export default function headerMenu() {
     return (
-        <main className='index dark-grey'>
-            <div className='navbar'>
-                <div className='logo'>
-                    <Image src={Logo} alt="Logo" width={120} height={40} priority />
-                </div>
-                <div className='menu'>
-                    <div className='menu-item'><Link href="/HouseDesigns">House Designs</Link></div>
-                    <div className='menu-item'>Your Stories</div>
-                    <div className='menu-item'>Resource Centre</div>
-                    <div className='menu-item'>Builders Directory</div>
-                    <div className='menu-item'>Contact</div>
-                    <div className='search-item'>
-                        <Image src={SearchGrey} alt="SearchGrey" width={15} height={15} priority />
-                    </div>
-                </div>
-            </div>
+        <main>
         </main>
     )
 }
