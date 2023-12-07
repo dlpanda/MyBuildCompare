503e052d906ffe604306774cd29fc8e544c0b7a1
潘嘉岩 <jiayan.pan@ctvic.cn>
2023-12-07 14:08:01 +0800

    修复vercel打包报错

--- src/components/HeadMenu.tsx
+++ src/components/HeadMenu.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from "next/link"
+import { useState } from 'react';
 // 图片
 import Logo from '../assets/index/logo.png'
 import SearchGrey from '../assets/icon/search-grey.png'
 import '../style/index.css'
 
 export default function headerMenu() {
+    const [searchValue, setsearchValue] = useState('');
     return (
         <main className='index dark-grey'>
             <div className='navbar'>
@@ -19,7 +21,9 @@ export default function headerMenu() {
                     <div className='menu-item'>Builders Directory</div>
                     <div className='menu-item'>Contact</div>
                     <div className='search-item'>
-                        <input className='grey' type="text" defaultValue={searchValue} placeholder='Search Category 2...'/>
+                        <input className='grey' type="text" value={searchValue} placeholder='Search Category 2...' onChange={e => {
+                            setsearchValue(e.target.value);
+                        }} />
                         <Image src={SearchGrey} alt="SearchGrey" width={15} height={15} priority />
                     </div>
                 </div>
