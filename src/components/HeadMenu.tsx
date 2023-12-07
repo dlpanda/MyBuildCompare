6ae8da5ed2ef5ec6ac0d3bf2c590c55d042bb598
潘嘉岩 <jiayan.pan@ctvic.cn>
2023-12-07 14:22:12 +0800

    修复打包错误

--- src/components/HeadMenu.tsx
+++ src/components/HeadMenu.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import Link from "next/link"
-import { useState } from 'react';
 // 图片
 import Logo from '../assets/index/logo.png'
 import SearchGrey from '../assets/icon/search-grey.png'
 import '../style/index.css'
 
 export default function headerMenu() {
-    const [searchValue, setsearchValue] = useState('');
     return (
         <main className='index dark-grey'>
             <div className='navbar'>
@@ -21,9 +19,6 @@ export default function headerMenu() {
                     <div className='menu-item'>Builders Directory</div>
                     <div className='menu-item'>Contact</div>
                     <div className='search-item'>
-                        <input className='grey' type="text" value={searchValue} placeholder='Search Category 2...' onChange={e => {
-                            setsearchValue(e.target.value);
-                        }} />
                         <Image src={SearchGrey} alt="SearchGrey" width={15} height={15} priority />
                     </div>
                 </div>
