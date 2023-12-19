3a35d42d00cb012b76bda82bdfc6a936ebde7d2c
DL_Panda <906772005@qq.com>
2023-12-19 20:14:14 +0800

    代码提交

--- src/components/MybuildCompare/SliderScroll copy.tsx
+++ src/components/MybuildCompare/SliderScroll copy.tsx
@@ -1,5 +1,9 @@
 // 首页滚动图片
 import React, { useState, useRef, useEffect } from "react";
+import silder1 from '@/assets/index/slider-1.png'
+import silder2 from '@/assets/index/slider-2.png'
+import silder3 from '@/assets/index/slider-3.png'
+import Carousel from '@/components/MybuildCompare/Carousel';
 export default function Slider() {
     const [scrollFlag, setscrollFlag] = useState(true);
     const [timer, settimer] = useState(0);
@@ -77,6 +81,7 @@ export default function Slider() {
                 <div className='left-arrow'></div>
                 <div className='right-arrow'></div>
             </div>
+            {/* <Carousel data={data}></Carousel> */}
             <div className='scroll-content' ref={scrollDom}>
                 <div className='content dark-grey semi-bold' ref={scrollContent}>
                     <div className='slider-item'>
--- src/utils/DataList.ts
+++ src/utils/DataList.ts
@@ -94,42 +94,48 @@ export const DataList = [{
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 }, {
     logoSrc: logo,
     isCollect: false,
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 }, {
     logoSrc: logo,
     isCollect: false,
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 }, {
     logoSrc: logo,
     isCollect: false,
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 }, {
     logoSrc: logo,
     isCollect: false,
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 }, {
     logoSrc: logo,
     isCollect: false,
     carouselImgSrc: [house, house, house],
     title: 'Cape Byron 245',
     text: "4 Bed, 2 bath, 2 Garage, 26 Squares",
-    author: "More Details"
+    author: "More Details",
+    quickLook:[]
 },]
 
 export const BreadcrumbData = [{
