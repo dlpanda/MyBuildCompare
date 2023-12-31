6544f722ab5de14163df973f7bf7b694b792c874
DL_Panda <906772005@qq.com>
2023-12-31 09:36:16 +0800

    删除多余组件

--- src/components/common/Text/BodyTextGrey.tsx
+++
@@ -1,9 +0,0 @@
-type TextProps = {
-  text: string;
-  className?: string;
-};
-export default function Text(props: TextProps) {
-  return (
-    <div className={`body-text-grey ${props.className}`}>{props.text}</div>
-  );
-}
--- src/components/common/Text/BodyTextLight.tsx
+++
@@ -1,9 +0,0 @@
-type TextProps = {
-  text: string;
-  className?: string;
-};
-export default function Text(props: TextProps) {
-  return (
-    <div className={`body-text-light ${props.className}`}>{props.text}</div>
-  );
-}
